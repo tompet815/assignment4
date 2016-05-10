140971ea23fd0f5c758c80a65081c971cb68c7b4
tomoe <cph-tp95@cphbusiness.dk>
2016-05-10 09:12:52 +0200

    added clearAllbooking in mainThreadTeest

--- test/FlightBookerMainThreadTest.java
+++ test/FlightBookerMainThreadTest.java
@@ -15,16 +15,29 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * this test class is just for running whole program
+ * this test class is just for running wholeprogram
+ *
  * @author Tomoe
  */
 public class FlightBookerMainThreadTest {
+
+    Controller con;
+    private final String plane_no = "CR9";
+    private final String USERNAME = "cphtp95";
+    private final String PW = "cphtp95";
+
+    @Before
+    public void setUp() {
+
+        con = new Controller(USERNAME, PW);
+        con.clearAllBookings(plane_no);
+    }
 
     @Test
     public void testRun() {
         MasterThread th = new MasterThread();
         th.run();
-      
+
     }
 
 }
